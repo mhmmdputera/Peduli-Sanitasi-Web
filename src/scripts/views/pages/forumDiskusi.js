6ea699b8fdd4351d4faa3b0d97cce41e0405195c
mhmmdputera <126052984+mhmmdputera@users.noreply.github.com>
2023-05-30 15:38:07 -0800

    halaman forum diskusi

---
+++ src/scripts/views/pages/forumDiskusi.js
@@ -0,0 +1,13 @@
+const ForumDiskusi = {
+    async render() {
+      return `
+      <discussion-form></discussion-form>
+      `;
+    },
+   
+    async afterRender() {
+      // Fungsi ini akan dipanggil setelah render()
+    },
+  };
+   
+  export default ForumDiskusi;
