4a3dd0e81b413c0ff8412ce91a075910cacb1245
Alfi010 <125624505+Alfi010@users.noreply.github.com>
2023-05-30 23:45:02 +0800

    Update routes.js
    Penambahan Article Page

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,10 +1,12 @@
 import Home from '../views/pages/home';
 import About from '../views/pages/about';
+import Article from '../views/pages/article';
 
 
 const routes = {
   '/': Home, // default page
   '/about': About,
+  '/article': Article,
 
 
 };
