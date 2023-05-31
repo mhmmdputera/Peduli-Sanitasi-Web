28b4828f87989db0c238f638c7319d557739577c
Alfi010 <125624505+Alfi010@users.noreply.github.com>
2023-05-31 23:20:20 +0800

    detailArticle.js
    Penambahan file detailArticle.js ke dalam folder pages

---
+++ src/scripts/views/pages/detailArticle.js
@@ -0,0 +1,13 @@
+const detailArticle = {
+    async render() {
+      return `
+       <detail-article></detail-article>
+      `;
+    },
+   
+    async afterRender() {
+      // Fungsi ini akan dipanggil setelah render()
+    },
+  };
+   
+  export default detailArticle;
