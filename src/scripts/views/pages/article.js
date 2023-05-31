54ea5b566b9ff555f139aaf47724a7daad6b82f6
Putri Nabella <75226486+Putrinaa@users.noreply.github.com>
2023-05-31 09:27:53 +0800

    Update navbar

--- src/scripts/views/pages/article.js
+++ src/scripts/views/pages/article.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import '../components/article-page';
 
 const Article = {
@@ -13,20 +12,4 @@ const Article = {
     },
   };
    
-=======
-import '../components/article-page';
-
-const Article = {
-    async render() {
-      return `
-       <article-page></article-page>
-      `;
-    },
-   
-    async afterRender() {
-      // Fungsi ini akan dipanggil setelah render()
-    },
-  };
-   
->>>>>>> 0284cc6583e810b260f8cd4f4b8a4cee2262b078
   export default Article;
--- src/scripts/views/pages/forumDiskusi.js
+++ src/scripts/views/pages/forumDiskusi.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const ForumDiskusi = {
     async render() {
       return `
@@ -11,18 +10,4 @@ const ForumDiskusi = {
     },
   };
    
-=======
-const ForumDiskusi = {
-    async render() {
-      return `
-      <discussion-form></discussion-form>
-      `;
-    },
-   
-    async afterRender() {
-      // Fungsi ini akan dipanggil setelah render()
-    },
-  };
-   
->>>>>>> 0284cc6583e810b260f8cd4f4b8a4cee2262b078
   export default ForumDiskusi;
