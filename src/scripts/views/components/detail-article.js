60e461cc1dcc7a344a2c2e82fa47918c2207930e
Alfi010 <125624505+Alfi010@users.noreply.github.com>
2023-05-31 23:17:40 +0800

    detail-article.js
    Menambahkan file detail-article.js ke dalam folder components

---
+++ src/scripts/views/components/detail-article.js
@@ -0,0 +1,69 @@
+// class DetailArticle extends HTMLElement {
+//     connectedCallback() {
+//       this.render();
+//     }
+  
+//     render() {
+//       this.innerHTML = `
+//         <div class="container">
+//             <div class="detail_article" id="detail_article" tabindex="0"></div>
+//         </div>
+//       `;
+//     }
+//   }
+  
+//   customElements.define('detail-article', DetailArticle);
+import articledataJson from '../../../articleData.json';
+
+class DetailArticle extends HTMLElement {
+    connectedCallback() {
+      this.render();
+      this.fetchArticleData();
+    }
+  
+    render() {
+      this.innerHTML = `
+        <div class="container">
+          <div class="detail_article" id="detail_article" tabindex="0"></div>
+        </div>
+      `;
+    }
+  
+    fetchArticleData() {
+      const articleId = this.getArticleIdFromURL();
+      const articleData = this.getArticleDataById(articleId);
+      if (articleData) {
+        const detailArticle = this.generateDetailArticleHTML(articleData);
+        document.getElementById('detail_article').innerHTML = detailArticle;
+      } else {
+        console.log('Artikel tidak ditemukan.');
+      }
+    }
+  
+    getArticleIdFromURL() {
+      const url = window.location.href;
+      const urlParts = url.split('/');
+      return urlParts[urlParts.length - 1];
+    }
+  
+    getArticleDataById(articleId) {
+      const items = articledataJson.artikel;
+      return items.find(item => item.id === articleId);
+    }
+  
+    generateDetailArticleHTML(articleData) {
+      const description = Object.values(articleData.content.description)
+        .map(p => `<p class="description">${p}</p>`)
+        .join('');
+  
+      return `
+        <h1>${articleData.title}</h1>
+        <p class="author">Penulis : <span>${articleData.author}</span> (${articleData.date})</p>
+        <img src="${articleData.image}" alt="${articleData['image-name']}" class="img-fluid">
+        <div class="deskripsi">${description}</div>
+      `;
+    }
+  }
+  
+  customElements.define('detail-article', DetailArticle);
+  
