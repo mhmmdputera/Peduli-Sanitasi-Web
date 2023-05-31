042e372e085b1756fc620d9cc1542ec78f85ed8d
Alfi010 <125624505+Alfi010@users.noreply.github.com>
2023-05-31 23:15:49 +0800

    Update routes.js
    Penambahan Routes untuk Halaman Detail Artikel

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,12 +2,14 @@ import Home from '../views/pages/home';
 import About from '../views/pages/about';
 import Article from '../views/pages/article';
 import ForumDiskusi from '../views/pages/forumDiskusi';
+import detailArticle from '../views/pages/detailArticle';
 
 const routes = {
   '/': Home, // default page
   '/about': About,
   '/article': Article,
-  '/diskusi': ForumDiskusi
+  '/article/:id': detailArticle,
+  '/diskusi': ForumDiskusi,
 
 };
  
