26ef443a8dfe2c3c21ebec8330e0a2047a0dfed9
Alfi010 <125624505+Alfi010@users.noreply.github.com>
2023-06-13 19:40:06 +0800

    Update detail-article.js

--- src/scripts/views/components/detail-article.js
+++ src/scripts/views/components/detail-article.js
@@ -1,54 +1,54 @@
 import articledataJson from '../../../articleData.json';
+import $ from 'jquery';
 
 class DetailArticle extends HTMLElement {
     connectedCallback() {
-      this.render();
-      this.fetchArticleData();
+        this.render();
+        this.fetchArticleData();
     }
-  
+
     render() {
-      this.innerHTML = `
-        <div class="container">
-          <div class="detail_article" id="detail_article" tabindex="0"></div>
-        </div>
-      `;
+        this.innerHTML = `
+            <div class="container">
+                <div class="detail_article" id="detail_article" tabindex="0"></div>
+            </div>
+        `;
     }
-  
+
     fetchArticleData() {
-      const articleId = this.getArticleIdFromURL();
-      const articleData = this.getArticleDataById(articleId);
-      if (articleData) {
-        const detailArticle = this.generateDetailArticleHTML(articleData);
-        document.getElementById('detail_article').innerHTML = detailArticle;
-      } else {
-        console.log('Artikel tidak ditemukan.');
-      }
+        const articleId = this.getArticleIdFromURL();
+        const articleData = this.getArticleDataById(articleId);
+        if (articleData) {
+            const detailArticle = this.generateDetailArticleHTML(articleData);
+            $('#detail_article').html(detailArticle);
+        } else {
+            console.log('Artikel tidak ditemukan.');
+        }
     }
-  
+
     getArticleIdFromURL() {
-      const url = window.location.href;
-      const urlParts = url.split('/');
-      return urlParts[urlParts.length - 1];
+        const url = window.location.href;
+        const urlParts = url.split('/');
+        return urlParts[urlParts.length - 1];
     }
-  
+
     getArticleDataById(articleId) {
-      const items = articledataJson.article;
-      return items.find(item => item.id === articleId);
+        const items = articledataJson.article;
+        return items.find(item => item.id === articleId);
     }
-  
+
     generateDetailArticleHTML(articleData) {
-      const description = Object.values(articleData.content.description)
-        .map(p => `<p class="description">${p}</p>`)
-        .join('');
-  
-      return `
-        <h1>${articleData.title}</h1>
-        <p class="author">Penulis : <span>${articleData.author}</span> (${articleData.date})</p>
-        <img src="${articleData.image}" alt="${articleData['image-name']}" class="img-fluid">
-        <div class="deskripsi">${description}</div>
-      `;
+        const description = Object.values(articleData.content.description)
+            .map(p => `<p class="description">${p}</p>`)
+            .join('');
+
+        return `
+            <h1>${articleData.title}</h1>
+            <p class="author">Penulis : <span>${articleData.author}</span> (${articleData.date})</p>
+            <img src="${articleData.image}" alt="${articleData['image-name']}" class="img-fluid">
+            <div class="deskripsi">${description}</div>
+        `;
     }
-  }
-  
-  customElements.define('detail-article', DetailArticle);
- 
+}
+
+customElements.define('detail-article', DetailArticle);
