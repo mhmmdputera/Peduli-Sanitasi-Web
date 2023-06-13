9b14ebfaa06d13c9845149cf70ab20044e6c3f0f
Alfi010 <125624505+Alfi010@users.noreply.github.com>
2023-06-13 12:07:51 +0800

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,10 +5,9 @@ import '../styles/forum.scss';
 import '../styles/animate.scss';
 import '../styles/flex-slider.scss';
 import '../styles/fontawesome.scss';
-import '../styles/owl.scss';
 import '../styles/detail-article.scss';
+import '../styles/other-article.scss';
 import '../styles/login-register-style.scss';
-import '../public/vendor/bootstrap/css/bootstrap.min.scss';
 import './views/components/navbar';
 import './views/components/main-banner';
 import './views/components/custom-footer';
@@ -19,14 +18,17 @@ import './views/components/article-section';
 import './views/components/about-team';
 import './views/components/article-page';
 import './views/components/detail-article';
+import './views/components/other-article';
 import './views/components/discussion-form';
 import './views/components/login-section'
 import './views/components/register-section'
 import '../articleData.json';
-import '../funFactsData';
+import '../funFactsData.json';
 import 'jquery';
-import 'owl.carousel';
-import '../public/js/owl-carousel'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'swiper/swiper-bundle.min.css';
+import '../../node_modules/swiper/swiper-bundle.min.js';
 import swRegister from './utils/sw-register';
 
 
@@ -45,19 +47,3 @@ window.addEventListener('load', () => {
   app.renderPage();
   swRegister();
 });
-
-// $('.owl-features').owlCarousel({
-//     center: true,
-//   items:2,
-//   loop:true,
-//   nav: true,
-//   margin:30,
-//   responsive:{
-//     992:{
-//         items:3
-//     },
-//     1200:{
-//         items:4
-//     }
-//   }
-// });
