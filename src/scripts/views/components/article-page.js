7ad9296265f066fe93342fd1dc1e6d994ddb9f2f
Alfi010 <125624505+Alfi010@users.noreply.github.com>
2023-06-13 12:08:19 +0800

    Update article-page.js

--- src/scripts/views/components/article-page.js
+++ src/scripts/views/components/article-page.js
@@ -1,45 +1,3 @@
-// import artikeldataJson from '../../../artikelData.json';
-
-// class ArticlePage extends HTMLElement {
-//   connectedCallback() {
-//     this.render();
-//   }
-
-//   render() {
-//     this.innerHTML = `
-//       <div class="container">
-//         <div class="article_page">
-//           <h1 class="title">DAFTAR ARTIKEL</h1>
-//           <div class="col-12">
-//             <div class="row gap-5 justify-content-center">
-//               <div class="col-lg-3 col-md-5 col-sm-9 border border-5 border-success rounded shadow">
-//               <div id="article_page"></div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       `;
-//     const items = artikeldataJson.artikel;
-//     const itemList = items.map(item => {
-//       const description = Object.values(item.konten.description).map(p => `<p class="description">${p}</p>`).join('');
-
-//       return `
-//     <div class="article_item" tabindex="0">
-//       <h1>${item.title}</h1>
-//       <p class="author">Penulis : <span>${item.author}</span> (${item.tanggal})</p>
-//       <img src="${item.image}" alt="${item['image-name']}">
-//       ${description.slice(0, 150)}
-//     </div>
-//   `;
-//     }).join('');
-
-//     document.getElementById('article_page').innerHTML = itemList;
-//   }
-// }
-
-// customElements.define('article-page', ArticlePage);
-
 import articledataJson from '../../../articleData.json';
 
 class ArticlePage extends HTMLElement {
@@ -60,10 +18,20 @@ class ArticlePage extends HTMLElement {
         </div>
       </div>
     `;
+    const articleLinks = this.querySelectorAll('a');
+    articleLinks.forEach(link => {
+      link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const target = event.target;
+        const href = target.getAttribute('href');
+        window.location.href = href;
+        window.scrollTo(0, 0);
+      });
+    });
   }
 
   generateArticleItems() {
-    const items = articledataJson.artikel;
+    const items = articledataJson.article;
     return items
       .map(item => {
         const description = Object.values(item.content.description)
@@ -71,7 +39,7 @@ class ArticlePage extends HTMLElement {
           .join('');
 
         return `
-          <div class="col-lg-3 col-md-5 col-sm-8 col-6 border border-5 border-success rounded shadow">
+          <div class="col-lg-3 col-md-5 col-sm-8 col-8 border border-5 border-success rounded shadow">
             <div class="article_item" tabindex="0">
               <h1>${item.title}</h1>
               <p class="author">Penulis : <span>${item.author}</span> (${item.date})</p>
