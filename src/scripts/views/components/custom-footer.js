9c9a7b81e25acfeb459889e6cebee2e28bd441ce
Putri Nabella <putrinabellaaa@gmail.com>
2023-06-14 09:35:06 +0800

    Update navbar dan footer

--- src/scripts/views/components/custom-footer.js
+++ src/scripts/views/components/custom-footer.js
@@ -1,6 +1,7 @@
 class CustomFooter extends HTMLElement {
     connectedCallback() {
       this.render();
+      this.addNavigationEventListeners();
     }
   
     render() {
@@ -18,7 +19,7 @@ class CustomFooter extends HTMLElement {
             <h2 class="useful_text">Menu</h2>
             <div class="footer_links">
               <ul class="ul-footer">
-                <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                <li class="scroll-to-section"><a href="#/" class="active">Home</a></li>
                 <li class="scroll-to-section"><a href="#/article">Article</a></li>
                 <li class="scroll-to-section"><a href="#/diskusi">Discussion Forum</a></li>
                 <li class="scroll-to-section"><a href="#/about">About</a></li>
@@ -37,6 +38,16 @@ class CustomFooter extends HTMLElement {
     </footer>
       `;
     }
+    
+    addNavigationEventListeners() {
+      const navigationLinks = this.querySelectorAll('.scroll-to-section a');
+      navigationLinks.forEach((link) => {
+        link.addEventListener('click', () => {
+          window.scrollTo(0, 0);
+        });
+      });
+    }
+
   }
   
   customElements.define('custom-footer', CustomFooter);
--- src/scripts/views/components/navbar.js
+++ src/scripts/views/components/navbar.js
@@ -1,34 +1,45 @@
 class NavBar extends HTMLElement {
   connectedCallback() {
     this.render();
+    this.addNavigationEventListeners();
   }
 
   render() {
     this.innerHTML = `
-    <header class="app-bar">
-      <div class="app-bar__menu">
-      <button id="button-container" class="button-container">
-      <img tabindex="0" aria-label="tombol-buka-menu-navigasi" src="icons/list.svg"
->
-  </button>
-      </div>
-      <div class="app-bar__brand" id="content">
-        <img src="./images/Logo Navbar - No Background.png" alt="Logo Peduli Sanitasi" />
-      </div>
-      <nav id="navigationDrawer" class="app-bar__navigation">
-        <ul>
-        <li class="app-bar__item"><a href="#/" class="active">Home</a></li>
-        <li class="app-bar__item"><a href="#/article">Article</a></li>
-        <li class="app-bar__item"><a href="#/diskusi">Discussion Forum</a></li>
-        <li class="app-bar__item"><a href="#/about">About</a></li>
-        <li class="app-bar__item"><a href="#/login">Log In</a></li>
-          </li>
-        </ul>
-      </nav>
-    </header>
-   
+      <header class="app-bar">
+        <div class="app-bar__menu">
+          <button id="button-container" class="button-container">
+            <img tabindex="0" aria-label="tombol-buka-menu-navigasi" src="icons/list.svg" />
+          </button>
+        </div>
+        <div class="app-bar__brand" id="content">
+          <img src="./images/Logo Navbar - No Background.png" alt="Logo Peduli Sanitasi" />
+        </div>
+        <nav id="navigationDrawer" class="app-bar__navigation">
+          <ul>
+            <li class="app-bar__item"><a href="#/" class="active">Home</a></li>
+            <li class="app-bar__item"><a href="#/article">Article</a></li>
+            <li class="app-bar__item"><a href="#/diskusi">Discussion Forum</a></li>
+            <li class="app-bar__item"><a href="#/about">About</a></li>
+            <li class="app-bar__item"><a href="#/login">Log In</a></li>
+          </ul>
+        </nav>
+      </header>
     `;
-    const navbar = document.querySelector('.app-bar');
+    this.addScrollEventListeners();
+  }
+
+  addNavigationEventListeners() {
+    const navigationLinks = this.querySelectorAll('.app-bar__item a');
+    navigationLinks.forEach((link) => {
+      link.addEventListener('click', () => {
+        window.scrollTo(0, 0);
+      });
+    });
+  }
+
+  addScrollEventListeners() {
+    const navbar = this.querySelector('.app-bar');
     window.addEventListener('scroll', function () {
       const scrollPosition = window.scrollY;
       if (scrollPosition > 0) {
