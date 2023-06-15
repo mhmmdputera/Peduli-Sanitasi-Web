266eca821b2fb5e42ead33a6f0892bcca923cc4d
Alfi010 <125624505+Alfi010@users.noreply.github.com>
2023-06-15 21:25:00 +0800

    Upload vendor.js

---
+++ src/scripts/vendor.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'swiper/swiper-bundle.min.css';
+import '../../node_modules/swiper/swiper-bundle.min.js';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
